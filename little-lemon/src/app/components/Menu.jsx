2edebcc27031026392509600fae4330737c45bd2
Dragos Manasca <dragos.manasca@essensys.ro>
2023-10-13 17:27:48 +0300

    web navbar menu and metatags

--- little-lemon/src/app/components/Menu.jsx
+++ little-lemon/src/app/components/Menu.jsx
@@ -1,10 +1,30 @@
 import * as React from "react";
+import { header_logo as Logo } from "../widgets/SVGs/index.jsx";
 
 export function Menu() {
   return (
-    <header className="menu">
-      <div></div>
-      <nav>Hello</nav>
+    <header className="navbar-menu">
+      <Logo />
+      <nav>
+        <a href="/" className="menu-item">
+          Home
+        </a>
+        <a href="/" className="menu-item">
+          About
+        </a>
+        <a href="/" className="menu-item">
+          Menu
+        </a>
+        <a href="/" className="menu-item">
+          Reservation
+        </a>
+        <a href="/" className="menu-item">
+          Order Online
+        </a>
+        <a href="/" className="menu-item">
+          Login
+        </a>
+      </nav>
     </header>
   );
 }
