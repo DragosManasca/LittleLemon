c08921001d5c26af38673c64f83e9c3d4971b988
Dragos Manasca <dragos.manasca@essensys.ro>
2023-10-16 14:29:01 +0300

    hero responsive styling

--- little-lemon/src/app/components/Hero.jsx
+++ little-lemon/src/app/components/Hero.jsx
@@ -3,10 +3,20 @@ import * as React from "react";
 export function Hero({ heroImage }) {
   return (
     <div className="hero">
-      <section>
+      <div className="hero-header">
         <h1 className="title">Little Lemon</h1>
-      </section>
-      <img src={heroImage} alt="restaurant content" />
+        <h2 className="subtitle">Chicago</h2>
+      </div>
+      <div className="hero-content">
+        <section className="hero-info">
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button className="btn btn-primary rounded">Reserve a table</button>
+        </section>
+        <img src={heroImage} alt="restaurant content" className="hero-image" />
+      </div>
     </div>
   );
 }
