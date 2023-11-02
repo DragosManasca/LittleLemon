858fc234a871b38412bc48aff22b0d5d845e404f
Dragos Manasca <dragos.manasca@essensys.ro>
2023-11-02 20:10:08 +0200

    Completed form unit tests

--- little-lemon/src/app/_tests_/Booking.test.js
+++ little-lemon/src/app/_tests_/Booking.test.js
@@ -45,9 +45,155 @@ describe("Booking Form", () => {
     const nameInput = screen.getByLabelText("Name*");
     fireEvent.change(nameInput, { target: { value: "" } });
 
+    const guestsInput = screen.getByLabelText("Number of guests*");
+    fireEvent.change(guestsInput, { target: { value: "5" } });
+
     const submitButton = screen.getByText("Make Your Reservation");
     fireEvent.click(submitButton);
 
     expect(submitButton).toHaveAttribute("disabled");
+    expect(submitButton).toBeDisabled();
+  });
+
+  test("Submission is disabled if user enters a name longer than 20 characters", () => {
+    const availableTimes = ["17:00", "18:00", "19:00", "20:00"];
+    const formData = {
+      name: "",
+      date: "2023-11-05",
+      time: availableTimes[0],
+      guests: 0,
+      occasion: "Anniversary",
+    };
+    const mockSetFormData = jest.fn();
+
+    render(
+      <ReactContextContainer>
+        <MemoryRouter>
+          <BookingForm
+            formData={formData}
+            setFormData={mockSetFormData}
+            availableTimes={availableTimes}
+          />
+        </MemoryRouter>
+      </ReactContextContainer>
+    );
+
+    const nameInput = screen.getByLabelText("Name*");
+    fireEvent.change(nameInput, {
+      target: { value: "some text longer than twenty characters" },
+    });
+
+    const guestsInput = screen.getByLabelText("Number of guests*");
+    fireEvent.change(guestsInput, { target: { value: "5" } });
+
+    const submitButton = screen.getByText("Make Your Reservation");
+    fireEvent.click(submitButton);
+
+    expect(submitButton).toHaveAttribute("disabled");
+    expect(submitButton).toBeDisabled();
+  });
+
+  test("Submission is disabled if user enters a guest number bigger than 10", () => {
+    const availableTimes = ["17:00", "18:00", "19:00", "20:00"];
+    const formData = {
+      name: "",
+      date: "2023-11-05",
+      time: availableTimes[0],
+      guests: 0,
+      occasion: "Anniversary",
+    };
+    const mockSetFormData = jest.fn();
+
+    render(
+      <ReactContextContainer>
+        <MemoryRouter>
+          <BookingForm
+            formData={formData}
+            setFormData={mockSetFormData}
+            availableTimes={availableTimes}
+          />
+        </MemoryRouter>
+      </ReactContextContainer>
+    );
+
+    const nameInput = screen.getByLabelText("Name*");
+    fireEvent.change(nameInput, {
+      target: { value: "Name" },
+    });
+
+    const guestsInput = screen.getByLabelText("Number of guests*");
+    fireEvent.change(guestsInput, { target: { value: "11" } });
+
+    const submitButton = screen.getByText("Make Your Reservation");
+    fireEvent.click(submitButton);
+
+    expect(submitButton).toHaveAttribute("disabled");
+    expect(submitButton).toBeDisabled();
+  });
+
+  test("Submission is disabled if there are no available tables on that day", () => {
+    const availableTimes = [];
+    const formData = {
+      name: "",
+      date: "2023-11-05",
+      time: availableTimes[0],
+      guests: 0,
+      occasion: "Anniversary",
+    };
+    const mockSetFormData = jest.fn();
+
+    render(
+      <ReactContextContainer>
+        <MemoryRouter>
+          <BookingForm
+            formData={formData}
+            setFormData={mockSetFormData}
+            availableTimes={availableTimes}
+          />
+        </MemoryRouter>
+      </ReactContextContainer>
+    );
+
+    const nameInput = screen.getByLabelText("Name*");
+    fireEvent.change(nameInput, {
+      target: { value: "Name" },
+    });
+
+    const guestsInput = screen.getByLabelText("Number of guests*");
+    fireEvent.change(guestsInput, { target: { value: "8" } });
+
+    const submitButton = screen.getByText("Make Your Reservation");
+    fireEvent.click(submitButton);
+
+    expect(submitButton).toHaveAttribute("disabled");
+    expect(submitButton).toBeDisabled();
+  });
+
+  test("Submission is enabled if all the form is correctly completed", () => {
+    const availableTimes = ["17:00", "18:00", "19:00", "20:00"];
+    const formData = {
+      name: "Name",
+      date: "2023-11-05",
+      time: availableTimes[0],
+      guests: 7,
+      occasion: "Anniversary",
+    };
+    const mockSetFormData = jest.fn();
+
+    render(
+      <ReactContextContainer>
+        <MemoryRouter>
+          <BookingForm
+            formData={formData}
+            setFormData={mockSetFormData}
+            availableTimes={availableTimes}
+          />
+        </MemoryRouter>
+      </ReactContextContainer>
+    );
+
+    const submitButton = screen.getByText("Make Your Reservation");
+
+    expect(submitButton).not.toBeDisabled();
   });
 });
